a5a189e5c8aaa743e1c44dc25f1d17e9bba3594b
EsraaMahmoudMohamed <esraamahmoud378@gmail.com>
2017-03-17 23:58:40 +0200

    first version for login by facebook with saving user data in database & insert not real data for email&pass&name in arabic for test

--- mZone/application/controllers/UserController.php
+++ mZone/application/controllers/UserController.php
@@ -2,10 +2,40 @@
 
 class UserController extends Zend_Controller_Action
 {
-
+		public $fpS;
     public function init()
     {
         /* Initialize action controller here */
+
+
+
+
+
+       $authorization = Zend_Auth::getInstance();
+        $fbsession = new Zend_Session_Namespace('facebook');
+
+        $this->fpS = new Zend_Session_Namespace('facebook');
+
+        $request=$this->getRequest();
+        $actionName=$request->getActionName();
+
+       //    if ((!$authorization->hasIdentity() && !isset($fbsession->first_name)) && ($actionName != 'login' && $actionName != 'fblogin' && $actionName !='fbcallback'))
+       //    {
+
+       //        $this->redirect('/user/login');
+       //    }
+
+
+       //    if (($authorization->hasIdentity() || isset($fbsession->first_name)) && ($actionName == 'login' || $actionName == 'fblogin'))
+       //    {
+            // $this->redirect('/user/home');
+       //    }
+
+   
+
+
+
+
 
 
     }
@@ -42,7 +72,7 @@ public function homeAction()
 
 				$user_model = new Application_Model_User();
 				$user_model->Register($request->getParams());
-				$this->redirect("/user/add");
+				$this->redirect("/user/home");
 			}
 		}
 	}// end add action 
@@ -110,16 +140,124 @@ public function homeAction()
           {
 		    $auth=Zend_Auth::getInstance();
 		    $auth->clearIdentity();
-		    // Zend_Session::namespaceUnset('facebook');
+		    Zend_Session::namespaceUnset('facebook');
 		    return $this->redirect('user/login');
 
 
        }
 
+//---------------------------------------------
+
+   public function fbloginAction()
+{
+   
+
+$fb = new Facebook\Facebook([
+'app_id' => '1309536932472952', // Replace {app-id} with your app id
+'app_secret' => '54d56b6a83dbb448a42a4b26e26ba401',
+'default_graph_version' => 'v2.2',
+]);
+$helper = $fb->getRedirectLoginHelper();
+$loginUrl = $helper->getLoginUrl($this->view->serverUrl().'/user/fbcallback');
+$this->view->facebook_url = $loginUrl;
+
+}
+
+//--------------------------------------------------------
+public function fbcallbackAction()
+{
+$fb = new Facebook\Facebook([
+'app_id' => '1309536932472952', // Replace {app-id} with your app id
+'app_secret' => '54d56b6a83dbb448a42a4b26e26ba401',
+'default_graph_version' => 'v2.2',
+]);
+
+$helper = $fb->getRedirectLoginHelper();
+
+try {
+$accessToken = $helper->getAccessToken();
+} catch(Facebook\Exceptions\FacebookResponseException $e) {
+// When Graph returns an error
+echo 'Graph returned an error: ' . $e->getMessage();
+exit;
+} catch(Facebook\Exceptions\FacebookSDKException $e) {
+// When validation fails or other local issues
+echo 'Facebook SDK returned an error: ' . $e->getMessage();
+exit;
+}
+
+if (! isset($accessToken)) {
+if ($helper->getError()) {
+header('HTTP/1.0 401 Unauthorized');
+echo "Error: " . $helper->getError() . "\n";
+echo "Error Code: " . $helper->getErrorCode() . "\n";
+echo "Error Reason: " . $helper->getErrorReason() . "\n";
+echo "Error Description: " . $helper->getErrorDescription() . "\n";
+} else {
+header('HTTP/1.0 400 Bad Request');
+echo 'Bad request';
+}
+exit;
+}
+// The OAuth 2.0 client handler helps us manage access tokens
+$oAuth2Client = $fb->getOAuth2Client();
+
+if (! $accessToken->isLongLived()) {
+// Exchanges a short-lived access token for a long-lived one
+try {
+$accessToken = $oAuth2Client->getLongLivedAccessToken($accessToken);
+} catch (Facebook\Exceptions\FacebookSDKException $e) {
+echo "<p>Error getting long-lived access token: " . $helper->getMessage() . "</p>\n\n";
+exit;
+}
+
+echo '<h3>Long-lived</h3>';
+
+}
+$fb->setDefaultAccessToken($accessToken);
+
+try {
+$response = $fb->get('/me?fields=id,first_name,last_name,email,gender,middle_name,birthday');
+
+$userNode = $response->getGraphUser();
+var_dump($userNode);
+// $a=$user_profile->getProperty("email");
+// echo $a;
+// $graphObject = $response->getGraphGroup();
+// $femail = $response->getEmail('email');
+// var_dump($femail);
+ // exit;
+
+}
+catch (Facebook\Exceptions\FacebookResponseException $e) {
+// When Graph returns an error
+echo 'Graph returned an error: ' . $e->getMessage();
+Exit;
+}
+catch (Facebook\Exceptions\FacebookSDKException $e) {
+// When validation fails or other local issues
+echo 'Facebook SDK returned an error: ' . $e->getMessage();
+Exit;
+}
+
+
+  $this->fpS->first_name = $userNode['first_name'];
+  $this->fpS->last_name = $userNode['last_name'];
+  $this->fpS->middle_name = $userNode['middle_name'];
+  $this->fpS->gender = $userNode['gender'];
+ // $this->redirect('/user/home');
+
+
+                $user_model = new Application_Model_User();
+				$user_model->InsertFB($userNode->all());
+
+
+}
 
 
 
 
+//--------------------------------------
 
 }// end of class 
 
--- mZone/application/models/User.php
+++ mZone/application/models/User.php
@@ -18,4 +18,22 @@ class Application_Model_User extends Zend_Db_Table_Abstract
 	$row->save();
 
 	}
+
+
+
+
+	 function InsertFB($fbData)
+	 {
+
+// email and pass not correct just for test insert operation for facebook data
+    $row=$this->createRow();
+	$row->name=$fbData['first_name'];
+	$row->name_ar=$fbData['last_name'];
+	$row->username=$fbData['first_name'];
+	$row->email=$fbData['gender'];
+	$row->password=$fbData['gender'];
+	$row->save();
+
+
+	 }
 }
