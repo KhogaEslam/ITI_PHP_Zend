bc08a2c6e7f8b306ff83682631fd6a85cac8090c
salmaGaber <salmaalex200@gmail.com>
2017-03-18 19:59:33 +0200

    Salma and Esraa 's Work

---
+++ mZone/application/controllers/CategoryController.php
@@ -0,0 +1,75 @@
+<?php
+
+class CategoryController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        /* Initialize action controller here */
+    }
+
+    public function indexAction()
+    {
+        // action body
+    }
+
+    public function listAction()
+    {
+        $category_model = new Application_Model_Category();
+        $this->view->category = $category_model->listCategory();
+    }
+
+
+    public function detailsAction()
+    {
+        $category_model = new Application_Model_Category();
+        $cat_id = $this->_request->getParam("cat_id");
+        $category = $category_model->categoryDetails($cat_id);
+        $this->view->category = $category;
+    }
+
+    public function deleteAction()
+    {
+        // action body
+        $category_model = new Application_Model_Category();
+        $cat_id= $this->_request->getParam('cat_id');
+        $category_model->deleteCategory($cat_id);
+        $this->redirect("/category/list");
+    }
+
+    public function addAction()
+    {
+        // action body
+        $form = new Application_Form_Categoryform();
+        $request = $this->getRequest();
+        if($request->isPost()){
+            if($form->isValid($request->getPost())){
+                $category_model = new Application_Model_Category();
+                $category_model-> addNewcategory($request->getParams());
+                $this->redirect('/category/list');
+            }
+        }
+        $this->view->category_form = $form;
+    }
+
+
+    public function updateAction()
+    {
+        // action body
+        $form = new Application_Form_Categoryform();
+        $cat_id = $this->_request->getParam('cat_id');
+        $category_model = new Application_Model_Category();
+        $data = $category_model->categoryDetails($cat_id);
+        $form->populate($data);
+        $request = $this->getRequest();
+        if($request->isPost()){
+            if($form->isValid($request->getPost())){
+                $category_model = new Application_Model_Category();
+                $category_model->updateCategory($cat_id,$request->getParams());
+                $this->redirect("/category/list");
+
+            }
+        }
+        $this->view->category_form = $form;
+    }
+}
--- mZone/application/models/Category.php
+++ mZone/application/models/Category.php
@@ -1,8 +1,39 @@
 <?php
 
-class Application_Model_Category
+class Application_Model_Category extends Zend_Db_Table_Abstract
 {
     protected $_name = 'category';
+
+
+    function listCategory()
+    {
+        return $this->fetchAll()->toArray();
+    }
+    function deleteCategory($cat_id)
+    {
+        $this->delete("cat_id=$cat_id");
+    }
+    function categoryDetails($cat_id)
+    {
+        return $this->find($cat_id)->toArray()[0];
+    }
+    function updateCategory($cat_id,$formData){
+        $categoryData['parent']=$formData['parent'];
+        $categoryData['cname']=$formData['cname'];
+        $categoryData['cdesc']=$formData['cdesc'];
+
+        $this->update($categoryData,"cat_id=".$cat_id);
+    }
+
+    function addNewcategory($categoryData)
+    {
+        $row = $this->createRow();
+        $row->parent = $categoryData['parent'];
+        $row->cname= $categoryData['cname'];
+        $row->cdesc = $categoryData['cdesc'];
+        $row->save();
+    }
+
 
 }
 
