44b94a8efd449f3edcb01133ba54b9cc04f7c1eb
esraa <Esraaelmasry696@yahoo.com>
2017-03-23 20:12:27 +0200

    Esraa Hassan's Work

--- mZone/public/footer.php
+++
@@ -1,6 +0,0 @@
-<div class="navbar navbar-inverse navbar-fixed-bottom">
-    <div align="center">
-        <h3 class="navbar-text" style="float:none">Copyright © 2017 - Open Source
-        </h3>
-    </div>
-</div>
